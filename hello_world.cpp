bf95c3a4e05bda497a58f5c8e596bc999941a817
schau721 <artemyshch@gmail.com>
2026-04-08 03:39:58 +0300

    Комментарии переведны на русский язык

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // Запрашиваем имя пользователя
+int main()
+{
+    // Запрашиваем имя пользователя.
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
 
-    // Выводим приветствие
+    // Выводим приветствие в стандартный поток вывода.
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
