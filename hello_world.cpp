2df2b2314088b765254ea011013a9cec71385eff
schau721 <artemyshch@gmail.com>
2026-04-08 03:37:16 +0300

    Mozilla и clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main()
+int
+main()
 {
-    // Запрашиваем имя пользователя.
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+  // Запрашиваем имя пользователя
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
 
-    // Выводим приветствие в стандартный поток вывода.
-    std::cout << "Hello world from @" << name << std::endl;
-    return 0;
+  // Выводим приветствие в стандартный потом вывода.
+  std::cout << "Hello world from @" << name << std::endl;
+  return 0;
+>>>>>>> 30233c6 (Mozilla и clang-format)
 }
