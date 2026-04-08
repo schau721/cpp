03ba98945cf5632785a5ad8ce8627af7432ca7e0
schau721 <artemyshch@gmail.com>
2026-04-08 03:27:43 +0300

    Add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,12 @@
 #include <string>
 
 int main() {
+    // Запрашиваем имя пользователя
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
+
+    // Выводим приветствие
     std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }
